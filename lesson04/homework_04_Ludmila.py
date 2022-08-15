99598db1ce8ab87c6bd12b6fde0f5ea09d3b0dc5
fishcka <l.afanasenko@gmail.com>
2022-08-15 19:55:27 +0300

    lesson 4 (function, class)

--- lesson04/homework_04_Ludmila.py
+++ lesson04/homework_04_Ludmila.py
@@ -13,13 +13,13 @@ def multiples_counting(checked_value):
         else:
             if diapason % checked_value == 0:
                 count = count + 1
-    print("Кількість чисел що діляться на " + str(checked_value) + ": " + str(count))
+    return count
 
 
-multiples_counting(8)
-multiples_counting(0)
-multiples_counting(99)
-multiples_counting(13)
+print(multiples_counting(8))
+print(multiples_counting(0))
+print(multiples_counting(99))
+print(multiples_counting(13))
 
 print("=" * 30)
 print("Завдання 2")
@@ -29,22 +29,25 @@ print("   ", "*" * 3)
 def calculator(param1, param2, action="+"):
     if action == "+":
         result = param1 + param2
-        print("Сума чисел", param1, "і", param2, "дорівнює:", int(result))
+        return result
     elif action == "-":
         result = param1 - param2
-        print("Різниця чисел", param1, "і", param2, "дорівнює:", int(result))
+        return result
     elif action == "*":
         result = param1 * param2
-        print("Добуток чисел", param1, "і", param2, "дорівнює:", int(result))
+        return result
     elif action == "/":
         result = param1 / param2
-        print("Частка чисел", param1, "і", param2, "дорівнює:", int(result))
+        return result
+    else:
+        print("Введіть +, -, * або /")
 
 
-calculator(14, 28)
-calculator(63, 7, "/")
-calculator(89, 13, "-")
-calculator(5, 45, "*")
+print(calculator(14, 28))
+print(calculator(63, 7, "/"))
+print(calculator(89, 13, "-"))
+print(calculator(5, 45, "*"))
+print(calculator(10, 12, "%"))
 
 print("=" * 30)
 print("Завдання 3")
@@ -52,20 +55,19 @@ print("   ", "*" * 3)
 
 
 class MathAction:
+
     def square(self, param):
         result1 = param ** 2
-        print("Квадрат числа", param, "дорівнює:", result1)
         return result1
 
     def square_root(self, param):
         result2 = param ** 0.5
-        print("Корінь числа", param, "дорівнює:", result2)
         return result2
 
 
 math_action1 = MathAction()
-math_action1.square(8)
-math_action1.square(51)
-math_action1.square_root(11)
-math_action1.square_root(125)
+print(math_action1.square(8))
+print(math_action1.square(51))
+print(math_action1.square_root(11))
+print(math_action1.square_root(125))
 print("=" * 30)
